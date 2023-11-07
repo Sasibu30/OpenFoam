aa9afeb9572edc90666ef396eec36a755eca116b
Seunghwan <seunghwan1547@gmail.com>
2023-11-07 11:10:36 +0900

    Tutorial 2
    OverSet 격자를 사용하는 튜토리얼 참고 파일들
    
    동작 테스트를 안 했기 때문에 오류 발생 시 스스로 해결 필요

---
+++ overset_plate_fixed/allrun.sh
@@ -0,0 +1,58 @@
+# sed -i -e 's/\r$//' 스크립트파일명  => 실행 필요
+rm -rf plate/constant/polyMesh
+rm -rf background/constant/polyMesh
+#
+#background
+#
+cd background
+blockMesh -dict ./system/blockMeshDict
+topoSet -dict ./system/topoSetDict_Level1
+refineMesh -overwrite
+topoSet -dict ./system/topoSetDict_Level2
+refineMesh -overwrite
+rm -rf ./0
+rm ./constant/polyMesh/*Zones
+topoSet
+rm -rf ./constant/polyMesh/sets
+#
+#plate
+#
+cd ../plate
+#
+blockMesh
+transformPoints -rotate  '( (0 1 0) (1 1 0) )'
+#
+topoSet
+#
+# Add the cylinder mesh on background mesh
+#
+cd ../background
+mergeMeshes . ../plate -overwrite
+rm -rf ../overSimpleFoam/constant/polyMesh
+cp -r ./constant/polyMesh ../overSimpleFoam/constant/polyMesh
+
+Call solver
+
+cd ../overSimpleFoam
+rm -rf 0.[0-9] [1-9]* postPro* process*
+rm -rf 0 && cp -r 0.orig 0
+setFields
+foamDictionary system/controlDict -entry endTime -set 5000
+foamDictionary system/controlDict -entry deltaT -set 1
+foamDictionary system/controlDict -entry writeInterval -set 100
+foamDictionary system/fvSchemes -entry ddtSchemes.default -set 'steadyState'
+foamDictionary system/fvSolution -entry relaxationFactors.fields.p -set 0.3
+foamDictionary system/fvSolution -entry relaxationFactors.equations.U -set 0.3
+overSimpleFoam
+
+# rm -rf 0
+# mv 1000/ 0
+# rm -rf 0/uniform/
+# rm -rf 0.[0-9] [1-9]* postPro* process*
+# foamDictionary system/controlDict -entry endTime -set 0.5
+# foamDictionary system/controlDict -entry deltaT -set 1e-5
+# foamDictionary system/controlDict -entry writeInterval -set 0.1
+# foamDictionary system/fvSchemes -entry ddtSchemes.default -set 'backward'
+# foamDictionary system/fvSolution -entry relaxationFactors.fields.p -set 1
+# foamDictionary system/fvSolution -entry relaxationFactors.equations.U -set 1
+# overPimpleDyMFoam
